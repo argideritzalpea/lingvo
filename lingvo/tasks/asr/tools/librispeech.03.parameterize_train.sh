cbde1aa385a454e2275971f2dab28192e6717df0
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-13 23:47:40 +0000

    Add code flagged in issue #146

--- lingvo/tasks/asr/tools/librispeech.03.parameterize_train.sh
+++ lingvo/tasks/asr/tools/librispeech.03.parameterize_train.sh
@@ -18,7 +18,7 @@ set -eu
 
 . lingvo/tasks/asr/tools/librispeech_lib.sh
 
-mkdir -p "${ROOT}/train"
+# mkdir -p "${ROOT}/train"
 
 # To save space, we don't unpack to intermediate files. The first pass collects
 # all transcription files from the tarball. The second pass unpacks the audio,
@@ -26,12 +26,12 @@ mkdir -p "${ROOT}/train"
 # with the accompanying transcription from the first pass.
 
 # This takes about 10 minutes per set.
-for subset in train-clean-100 train-clean-360 train-other-500; do
-  echo "=== First pass, collecting transcripts: ${subset}"
-  python3 -m lingvo.tools.create_asr_features --logtostderr \
-    --input_tarball="${ROOT}/raw/${subset}.tar.gz" --dump_transcripts \
-    --transcripts_filepath="${ROOT}/train/${subset}.txt"
-done
+#for subset in train-clean-100 train-clean-360 train-other-500; do
+#  echo "=== First pass, collecting transcripts: ${subset}"
+#  python3 -m lingvo.tools.create_asr_features --logtostderr \
+#    --input_tarball="${ROOT}/raw/${subset}.tar.gz" --dump_transcripts \
+#    --transcripts_filepath="${ROOT}/train/${subset}.txt"
+#done
 
 # We are allocating as follows:
 #  num utts num_shards  subset
@@ -45,10 +45,11 @@ done
 
 # Second pass: Create tf.Examples. It takes about 90 minutes.
 
-rm -f FAILED
+#rm -f FAILED
 subset=train-clean-100
 echo "=== Second pass, parameterization: ${subset}"
 for subshard in $(seq 0 9); do
+  echo ${ROOT}/raw/${subset}.tar.gz
   set -x
   nice -n 20 python3 -m lingvo.tools.create_asr_features \
     --logtostderr \
@@ -62,6 +63,7 @@ done
 wait
 ! [ -f FAILED ]
 
+: '
 subset=train-clean-360
 echo "=== Second pass, parameterization: ${subset}"
 for subshard in $(seq 0 9); do
@@ -95,3 +97,4 @@ for subshard in $(seq 0 9); do
 done
 wait
 ! [ -f FAILED ]
+'
